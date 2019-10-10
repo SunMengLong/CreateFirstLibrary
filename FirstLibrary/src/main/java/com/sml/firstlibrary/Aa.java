2eba221c9f014296187f54ab14144422e401fece
sunmenglong <sunmenglong@startimes.com.cn>
2019-10-10 09:46:51 +0800

    提交库代码

---
+++ FirstLibrary/src/main/java/com/sml/firstlibrary/Aa.java
@@ -0,0 +1,13 @@
+package com.sml.firstlibrary;
+
+import android.util.Log;
+
+/**
+ * Created by 10007358 on 2019/10/10.
+ */
+
+public class Aa {
+    public static void sml(){
+        Log.i("sunml", "sml: ................112233");
+    }
+}
